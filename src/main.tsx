acd3ff3ff73469f31408e010ae92b47fb78b02f6
YangGwangSeong <rhkdtjd_12@naver.com>
2025-04-24 19:29:31 +0900

    feat: Amplitude init (#2)

--- src/main.tsx
+++ src/main.tsx
@@ -2,9 +2,12 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import { BrowserRouter } from 'react-router-dom';
 
+import { init } from '@amplitude/analytics-browser';
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
 
 import App from '@/App';
+
+init(import.meta.env.VITE_AMPLITUDE_API_KEY, { autocapture: true });
 
 const queryClient = new QueryClient({
 	defaultOptions: {
